5998b6aba41c4bf2eee6e0a868f59b69cf03a512
gbersani <bersani@Bersanis-MacBook-Air.local>
2016-04-21 12:01:09 -0400

    Adds a client-side email validation method

--- js/api/Email.js
+++ js/api/Email.js
@@ -7,9 +7,7 @@ let rest_config = {
 }
 
 export function verifyEmail(email_string, callback) {
-  //TODO: Validade email
-
-  console.log('verifyEmail')
+  //TODO: Use the validateEmailString function for client-side validation before sending a request.
 
   rest_config.body = JSON.stringify({
     email: email_string,
@@ -17,26 +15,30 @@ export function verifyEmail(email_string, callback) {
 
   fetch('http://api.goodquestion.io/email/verify', rest_config)  
     .then(function (response) {
-      console.log('original response: ')
-      console.log(response)
-
       // Fake endpoint responses for now.
       response = {
         email: email_string,
         verified: true
       }
-
       callback(response)
     })
     .catch(function (response) {
       // TODO: We will need to replace this with real error handling when our api endpoints are set.
       // For now we will return placeholder data.
-
       response = {
         email: email_string,
         verified: true
       }
-
       callback(response)
     })
+}
+
+/**
+ * Validates an email string using RegEx.
+ * @param  {string} email_string
+ * @return {bool}   result
+ */
+function validateEmailString(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
