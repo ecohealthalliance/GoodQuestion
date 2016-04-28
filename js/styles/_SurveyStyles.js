7e40f54647879388901e3e3d54531c19610afebf
dan-nyanko <dan-nyanko@users.noreply.github.com>
2016-04-28 18:19:22 -0400

    updating styles

--- js/styles/_SurveyStyles.js
+++ js/styles/_SurveyStyles.js
@@ -9,11 +9,27 @@ const SurveyStyles = StyleSheet.create({
     backgroundColor: Color.background2,
   },
   listitem: {
+    flex: 1,
+    flexDirection: 'row',
   	padding: 20,
-  	alignSelf: 'stretch',
     borderBottomWidth: 1,
     borderColor: Color.background1,
   },
+  checkbox: {
+   backgroundColor: '#eee',
+   borderRadius: 5,
+   height: 25,
+   width: 25,
+   borderWidth: 2,
+ },
+ title: {
+   fontSize: 18,
+   color: 'dimgray',
+ },
+ subtitle: {
+   fontSize: 12,
+   color: 'dimgray',
+ },
 })
 
 module.exports = SurveyStyles
