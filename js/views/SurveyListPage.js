e0748857663d7be1e34bc5ada45af4fd91be26d1
dan-nyanko <dan-nyanko@users.noreply.github.com>
2016-04-28 18:20:12 -0400

    styled the list, added checkbox, set the view title

--- js/views/SurveyListPage.js
+++ js/views/SurveyListPage.js
@@ -8,11 +8,14 @@ import React, {
   ListView,
 } from 'react-native'
 
+import _ from 'lodash'
 import Store from '../data/Store'
 import Styles from '../styles/Styles'
 
+import CheckBox from 'react-native-checkbox'
 
 const SurveyListPage = React.createClass ({
+  title: 'Surveys',
   getInitialState() {
     return {
       dataSource: new ListView.DataSource({
@@ -20,21 +23,41 @@ const SurveyListPage = React.createClass ({
       })
     }
   },
-
   componentDidMount() {
+    this.props.setTitle(this.title);
     this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(Store.surveys)
+      dataSource: this.state.dataSource.cloneWithRows(Store.surveys),
     })
   },
 
   /* Methods */
+  onChecked(rowId) {
+    let newSource = Store.surveys.slice();
+    let oldItem = Store.surveys[rowId];
+    oldItem.accepted = !oldItem.accepted;
+    newSource[rowId] = Object.assign({}, oldItem);
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(newSource),
+    });
+  },
 
   /* Render */
-
-  renderItem(item) {
+  renderItem(item, sectionId, rowId) {
     return (
       <View style={Styles.survey.listitem}>
-        <Text style={Styles.type.p}>{item.title}</Text>
+        <View style={Styles.container.col75}>
+          <Text style={Styles.survey.title}>{item.title}</Text>
+          <Text style={Styles.survey.subtitle}>A subtitle</Text>
+        </View>
+        <View style={[Styles.container.col25, {alignItems: 'flex-end'}]}>
+          <CheckBox
+            ref={item.objectId}
+            checked={item.accepted}
+            checkImage={require('react-native-checkbox/example-custom-check.png')}
+            onChange={this.onChecked.bind(this, rowId)}
+            style={Styles.survey.acceptedCheckbox}
+          />
+        </View>
       </View>
     )
   },
