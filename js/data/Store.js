e6ec62b61a10526b652fa33418e71a42a481deb5
dan-nyanko <dan-nyanko@users.noreply.github.com>
2016-04-28 18:17:45 -0400

    made some changes to the mock data

--- js/data/Store.js
+++ js/data/Store.js
@@ -1,5 +1,5 @@
 const Store = {
-  platform: 'android',
+  platform: null,
 
   logged_in: false,
   email_verified: false,
@@ -10,7 +10,7 @@ const Store = {
   },
 
   user: {
-    _id: 'SoMeH4sH',
+    objectId: 'SoMeH4sH',
     name: 'John Doe',
     email: 'john@doe.com',
     isAdmin: false,
@@ -18,78 +18,185 @@ const Store = {
 
   surveys: [
     {
-      _id: 'sur1',
+      objectId: 's1',
       title: 'Survey #1',
       user: 'user1',
       created: Date.now() - 10000,
-      forms: [ 1 ],
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
     },
     {
-      _id: 'sur2',
+      objectId: 's2',
       title: 'Survey #2',
       user: 'user1',
       created: Date.now() - 800000,
-      forms: [ 'form1', 'form2' ],
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
     },
     {
-      _id: 'sur2',
+      objectId: 's3',
       title: 'Survey #3',
       user: 'user1',
       created: Date.now() - 800000,
-      forms: [ 'form1', 'form2' ],
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
     },
-    { title: 'Survey #' + Math.floor(Math.random() * 1000) },
-    { title: 'Survey #' + Math.floor(Math.random() * 1000) },
-    { title: 'Survey #' + Math.floor(Math.random() * 1000) },
-    { title: 'Survey #' + Math.floor(Math.random() * 1000) },
-    { title: 'Survey #' + Math.floor(Math.random() * 1000) },
-    { title: 'Survey #' + Math.floor(Math.random() * 1000) },
-    { title: 'Survey #' + Math.floor(Math.random() * 1000) },
-    { title: 'Survey #' + Math.floor(Math.random() * 1000) },
-    { title: 'Survey #' + Math.floor(Math.random() * 1000) },
     {
-      _id: 'sur9',
-      title: 'Survey #Final',
+      objectId: 's4',
+      title: 'Survey #4',
       user: 'user1',
       created: Date.now() - 800000,
-      forms: [ 'form1', 'form2' ],
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
+    },
+    {
+      objectId: 's5',
+      title: 'Survey #5',
+      user: 'user1',
+      created: Date.now() - 10000,
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
+    },
+    {
+      objectId: 's6',
+      title: 'Survey #6',
+      user: 'user1',
+      created: Date.now() - 800000,
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
+    },
+    {
+      objectId: 's7',
+      title: 'Survey #7',
+      user: 'user1',
+      created: Date.now() - 800000,
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
+    },
+    {
+      objectId: 's8',
+      title: 'Survey #8',
+      user: 'user1',
+      created: Date.now() - 800000,
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
+    },
+    {
+      objectId: 's9',
+      title: 'Survey #9',
+      user: 'user1',
+      created: Date.now() - 10000,
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
+    },
+    {
+      objectId: 's10',
+      title: 'Survey #10',
+      user: 'user1',
+      created: Date.now() - 800000,
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
+    },
+    {
+      objectId: 's11',
+      title: 'Survey #11',
+      user: 'user1',
+      created: Date.now() - 800000,
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
+    },
+    {
+      objectId: 's12',
+      title: 'Survey #12',
+      user: 'user1',
+      created: Date.now() - 800000,
+      accepted: false,
+      forms: [ 'f1', 'f2' ],
     },
   ],
 
-  forms: {
-    form1: {
-      _id: 'form1',
+  forms: [
+    {
+      objectId: 'f1',
       triggers: [ 't1' ],
       questions: [ 'q1', 'q2', 'q3' ]
     }
-  },
+  ],
 
-  questions: {
-    q1: {
-      survey: 'sur1',
-      text: 'What is your favorite color?',
-      question_type: 'multi_choice',
+  questions: [
+    {
+      objectId: 'q1',
+      survey: 's1',
+      text: 'Tell a little about yourself',
+      value: '',
+      questionType: 'textArea',
       properties: {
-        choices: ['red', 'blue', 'green']
-      }
+        placeholder: 'Tell a little about yourself',
+      },
+      order: 2,
     },
-    q2: {
-      survey: 'sur1',
-      text: 'What is your favorite color?',
-      question_type: 'multi_choice',
+    {
+      objectId: 'q2',
+      survey: 's1',
+      text: 'What is your name?',
+      value: '',
+      questionType: 'inputText',
       properties: {
-        choices: ['red', 'blue', 'green']
-      }
+        placeholder: 'Please specify your full name',
+        maxlength: 20,
+        required: true,
+      },
+      order: 1,
     },
-    q3: {
-      survey: 'sur1',
-      text: 'What is your favorite color?',
-      question_type: 'multi_choice',
+    {
+      objectId: 'q3',
+      survey: 's1',
+      text: 'What is your favorite number?',
+      questionType: 'multipleChoice',
+      value: '',
       properties: {
-        choices: ['red', 'blue', 'green']
-      }
+        choices: [1, 2, 3],
+        required: false,
+      },
+      order: 3,
     },
-  },
+    {
+      objectId: 'q4',
+      survey: 's2',
+      text: 'Tell a little about yourself',
+      value: '',
+      questionType: 'textArea',
+      properties: {
+        placeholder: 'Tell a little about yourself',
+      },
+      order: 2,
+    },
+    {
+      objectId: 'q5',
+      survey: 's2',
+      text: 'What is your name?',
+      value: '',
+      questionType: 'inputText',
+      properties: {
+        placeholder: 'Please specify your full name',
+        maxlength: 20,
+        required: true,
+      },
+      order: 1,
+    },
+    {
+      objectId: 'q6',
+      survey: 's2',
+      text: 'What is your favorite number?',
+      questionType: 'multipleChoice',
+      value: '',
+      properties: {
+        choices: [1, 2, 3],
+        required: false,
+      },
+      order: 3,
+    },
+  ],
 
 }
 
