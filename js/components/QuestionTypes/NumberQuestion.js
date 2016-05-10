44d3d3b96dd4c122eea6cb14d27c8cc48c843727
gbersani <bersani@Bersanis-MacBook-Air.local>
2016-05-10 12:33:04 -0400

    Adds plus/minus buttons to NumberQuestion component

--- js/components/QuestionTypes/NumberQuestion.js
+++ js/components/QuestionTypes/NumberQuestion.js
@@ -6,6 +6,7 @@ import React, {
   Slider,
 } from 'react-native'
 import Styles from '../../styles/Styles'
+import Button from 'apsl-react-native-button'
 
 const NumberQuestion = React.createClass ({
   propTypes: {
@@ -32,17 +33,35 @@ const NumberQuestion = React.createClass ({
   getInitialState: function() {
     return {
       value: this.props.value,
+      valueText: String(this.props.value),
     }
   },
 
   /* Methods */
-  handleChange(value) {
-    if (value < this.props.properties.min) value = this.props.properties.min 
-    if (value > this.props.properties.max) value = this.props.properties.max
-    this.setState({
-      value: value
-    })
-    this.props.onChange(value)
+
+  // Note: The iOS TextInput display does not work well with the Number data type.
+  // It must be converted to a String to avoid rendering problems.
+  handleChange(valueText) {
+    let value = Number(valueText)
+    if (!isNaN(value)) {
+      if (value < this.props.properties.min) value = this.props.properties.min
+      if (value > this.props.properties.max) value = this.props.properties.max
+      this.setState({
+        valueText: String(value),
+        value: value,
+      })
+      this.props.onChange(value)
+    } else {
+      console.warn('Eror: ' + valueText + ' is not a valid number.')
+    }
+  },
+
+  increaseCount() {
+    this.handleChange(this.state.value + 1)
+  },
+
+  decreaseCount() {
+    this.handleChange(this.state.value - 1)
   },
 
   /* Render */
@@ -51,12 +70,20 @@ const NumberQuestion = React.createClass ({
     return (
       <View>
         <Text style={Styles.type.h1}>{this.props.text}</Text>
-        <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 100}}
-          onChangeText={this.handleChange}
-          keyboardType='numeric'
-          value={this.state.value}
-        />
+        <View style={Styles.form.doubleButtons}>
+          <Button onPress={this.decreaseCount} style={Styles.form.doubleButtonLeft}>
+            -
+          </Button>
+          <TextInput
+            style={{flex:1, height: 40, borderColor: 'gray', borderWidth: 1, textAlign: 'center'}}
+            onChangeText={this.handleChange}
+            keyboardType='numeric'
+            value={this.state.valueText}
+          />
+          <Button onPress={this.increaseCount} style={Styles.form.doubleButtonRight}>
+            +
+          </Button>
+        </View>
       </View>
     )
   }
