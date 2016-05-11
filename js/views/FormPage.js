7151463858223c3a1e025082b8f6842593fbb259
Abe Miessler <miessler@ecohealthalliance.org>
2016-05-11 14:50:57 -0700

    adding title to formPage

--- js/views/FormPage.js
+++ js/views/FormPage.js
@@ -17,6 +17,7 @@ import Button from 'apsl-react-native-button';
 
 import { loadQuestions } from '../api/Questions'
 
+
 const FormPage = React.createClass ({
   propTypes: {
     form: React.PropTypes.object.isRequired,
@@ -31,6 +32,7 @@ const FormPage = React.createClass ({
   },
 
   componentWillMount() {
+    this.props.setTitle("Survey: " + this.props.survey.get('title'));
     let id = this.genSubmissionKey();
     AsyncStorage.getItem(id, (err, res) => {
       if (res) {
