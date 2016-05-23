c56edd37ebd795b2b9417551fa74e25bd006b3d0
dan-nyanko <dan-nyanko@users.noreply.github.com>
2016-05-23 16:57:39 -0400

    updating realm schema

--- js/models/Submission.js
+++ js/models/Submission.js
@@ -4,6 +4,8 @@ export default class Submission {
 Submission.schema = {
   name: 'Submission',
   properties: {
+    uniqueId: 'string',
+    dirty: 'bool',
     created:  'date',
     formId: 'string',
     // Serialized JSON
