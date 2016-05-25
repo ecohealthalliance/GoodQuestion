ee7b223c04cea850fdfa8869914e82adbad0d2a4
dan-nyanko <dan-nyanko@users.noreply.github.com>
2016-05-25 07:21:28 -0400

    refactor java package name
    ignore .iml
    
    added chore about yellowbox warning in pivotal
    
    check platform === ios for PushNotificationIOS
    
    re-enable initializeGeoloationService
    
    add react-native-background-geoloaction-android package
    
    hardcoded strings, see pivotal #120595607
    
    adds dependencies for parse, see pivotal #120594229
    
    add Application.class
    
    create the parse-server installation object
    
    adds GoodQuestionApplication to initialize parse-server
    
    add 3rd party package for push notifications
    
    setting up android react-native-push-notification
    
    implement PushNotification.localNotification (cross platform)
    
    removing Objective-C Parse SDK
    
    removing Java Parse SDK
    
    cross platform upsertInstallation object to parse via REST API
    
    use private react-native-background-geolocation-android

---
+++ android/app/src/main/java/io/eha/goodquestion/MainActivity.java
@@ -0,0 +1,70 @@
+package io.eha.goodquestion;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.transistorsoft.rnbackgroundgeolocation.RNBackgroundGeolocation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.realm.react.RealmReactPackage;
+
+public class MainActivity extends ReactActivity {
+    private static final Logger logger = LoggerFactory.getLogger(MainActivity.class);
+    private ReactNativePushNotificationPackage mReactNativePushNotificationPackage;
+
+    public MainActivity() {
+    }
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "GoodQuestion";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+        return io.eha.goodquestion.BuildConfig.DEBUG;
+    }
+
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
+    @Override
+    protected List<ReactPackage> getPackages() {
+        mReactNativePushNotificationPackage = new ReactNativePushNotificationPackage(this);
+        return Arrays.<ReactPackage>asList(
+                new RNBackgroundGeolocation(this),      // <-- for background-geolocation
+                new MainReactPackage(),
+                new RealmReactPackage(), mReactNativePushNotificationPackage
+        );
+    }
+
+    // Add onNewIntent
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        mReactNativePushNotificationPackage.newIntent(intent);
+    }
+}
