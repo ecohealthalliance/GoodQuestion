42ae9a00aaed298a49af1e16175cb36e81cc63e0
dan-nyanko <dan.nyanko@gmail.com>
2016-06-22 14:49:55 -0400

    decoupled toast messages

---
+++ js/models/ValidObject.js
@@ -0,0 +1,43 @@
+import Joi from '../lib/joi-browser.min'
+
+/**
+ * ValidObject is a base class to be extended for validation purposes. It was
+ * created for use with non-realm models, but potentailly this could be
+ * implemented in the future to validate data comming in from the remote
+ * server.
+ */
+class ValidObject {
+  /**
+   * validates the object based on a schema
+   *
+   * @return {array} errors, an array of errors
+   */
+  validate() {
+    if (typeof this._schema === 'undefined') return ['Invalid Schema'];
+    const errors = [];
+    Object.keys(this._schema).forEach((key) => {
+      const value = this[key];
+      const obj = {};
+      obj[key] = value;
+      const singleSchema = {};
+      singleSchema[key] = this._schema[key];
+      const res = Joi.validate(obj, singleSchema);
+      if (res.hasOwnProperty('error') && res.error !== null) {
+        errors.push(res.error);
+      }
+    });
+    return errors;
+  }
+  /**
+   * determines if the object has any property errors by calling validate
+   *
+   * @return {boolean} valid
+   */
+  isValid() {
+    const errors = this.validate();
+    if (errors.length > 0) return false;
+    return true;
+  }
+}
+
+module.exports = ValidObject
--- js/styles/Styles.js
+++ js/styles/Styles.js
@@ -14,18 +14,19 @@ import HeaderStyles from './_HeaderStyles'
 import DrawerStyles from './_DrawerStyles'
 import ControlPanelStyles from './_ControlPanelStyles'
 import ProfileStyles from './_ProfileStyles'
+import ToastStyles from './_ToastStyles'
 
 const Styles = {
   container: ContainerStyles,
   type: TypeStyles,
   form: FormStyles,
-
   header: HeaderStyles,
   drawer: DrawerStyles,
   survey: SurveyStyles,
   controlPanel: ControlPanelStyles,
   question: QuestionStyles,
   profile: ProfileStyles,
+  toast: ToastStyles,
 }
 
 module.exports = Styles
