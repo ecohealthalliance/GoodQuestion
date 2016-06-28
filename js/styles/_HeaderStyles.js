014f7074f1abcd7fef69d79ef2bca3abd73716be
gbersani <bersani@Bersanis-Air.lan>
2016-06-28 09:55:51 -0400

    Fixes header padding on iOS

--- js/styles/_HeaderStyles.js
+++ js/styles/_HeaderStyles.js
@@ -15,7 +15,7 @@ const HeaderStyles = StyleSheet.create({
     left: 0,
     right: 0,
     height: Variables.HEADER_SIZE,
-    paddingTop: Variables.HEADER_PADDING,
+    paddingTop: 20,
     paddingHorizontal: 0,
     backgroundColor: Color.background1,
   },
@@ -57,14 +57,15 @@ const HeaderStyles = StyleSheet.create({
 
   navBarLeftButton: {
     flex: .1,
-    padding: 20,
-    paddingRight: 16,
+    padding: Platform.OS == 'ios' ? 14 : 20,
+    paddingRight: Platform.OS == 'ios' ? 14 : 16,
     marginRight: 2,
   },
   navBarRightButton: {
     flex: .1,
-    padding: 20,
-    paddingRight: 14,
+    padding: Platform.OS == 'ios' ? 14 : 20,
+    paddingRight: Platform.OS == 'ios' ? 10 : 14,
+
   },
 })
 
